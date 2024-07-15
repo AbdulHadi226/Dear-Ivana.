7b1e6451a77797bd43b864ba4293b25a32d6280f
AbdulHadi226 <161116357+AbdulHadi226@users.noreply.github.com>
2024-07-15 17:29:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeText() {
+    var button = document.getElementById('love-button');
+    if (button.innerText === 'Click this button') {
+        button.innerText = "I love you so so so so much. Mwah 💋";
+    } else {
+        button.innerText = "Click this button";
+    }
+}
